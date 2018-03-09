faeb6ebc704f9100e105ff27e7e2c85693f12dff
陈禹西 <30655304+Heriyadi235@users.noreply.github.com>
2018-03-09 23:08:18 +0800

    完成了我之前计划的
    功能已经实现了
    名字长的那个html是一个简单的路线创建页面

--- swtool.py
+++ swtool.py
@@ -1,15 +1,15 @@
 import math
-def drawline():
-    lon1 = 123.418949
+def drawline(lon1,lat1,lon2,lat2):
+    '''lon1 = 123.418949
     lat1 = 41.933305
     lon2 = 123.413523
-    lat2 = 41.933506
+    lat2 = 41.933506'''
     #y=ax+b
     a = (lon2-lon1)/(lat2-lat1)
     b = lat1-lon1*a
     distance = getdistance(lon1,lat1,lon2,lat2)
     print("Distance:"+str(distance)+"\n")
-    pointnum = int(distance/0.3) #30厘米出一次位置
+    pointnum = int(distance/10) #30厘米出一次位置,这里单位是m
     print("Pointnum:"+str(pointnum)+"\n")
     dx = (lon2-lon1)/pointnum
     dy = (lat2-lat1)/pointnum
@@ -20,6 +20,7 @@ def drawline():
         #print("Lon:"+str(x)+"\nLat:"+str(y)+"\n")
         print(str(x)+","+str(y)+"\n")
         i += 1
+    print(str(lon2)+","+str(lat2)+"\n")
 def getdistance(lon1,lat1,lon2,lat2):
     lon1 = math.radians(lon1)
     lat1 = math.radians(lat1)
@@ -34,7 +35,23 @@ def getdistance(lon1,lat1,lon2,lat2):
     distance = rad_dist* 3437.74677*1.1508*1.609347*1000
     return  distance
 if __name__=='__main__':
+    Slon1=123.418949
+    Slat1=41.933305
+    Elon2=000.000000
+    Elat2=00.000000
     try:
+        for line in open('E:\\addlist.txt'):
+            #123.123456,42.123345
+            #print("文章地址")
+            #print(line)
+            print("开始路程")
+            Elon2=float(line[0:9])#当前行经度
+            Elat2=float(line[11:19])#当前行纬度
+            print(str(Elon2)+","+str(Elat2)+"\n")
+            drawline(Slon1,Slat1,Elon2,Elat2)
+            Slon1=Elon2
+            Slon1=Elon2
+        print("跑步结束")
         drawline()
     except Exception as e:
         print(str(e))
